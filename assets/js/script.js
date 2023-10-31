3860e0b9b5fb318df7afbf7a93388b5a1b9c28f9
selamshim <shimeselam@gmail.com>
2023-10-31 14:17:34 +0100

    user name and instruction added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 //Variables
+//js first area
+
+const startArea = document.getElementById("start-area");
+const nameInput = document.getElementById("user-name");
+const rulesArea = document.getElementById("rules-area");
+const submitButton = document.getElementById("user-submit");
+const startQuiz = document.getElementById("start-quiz-btn");
+const formInput = document.querySelector(".form-input");
+const questionsArea = document.getElementById("questions-area");
+
+//js second area
 const quiz= document.getElementById('quiz');
 const answerEls = document.querySelectorAll('.quizAnswer');
 const questionEl = document.getElementById('question');
@@ -12,10 +23,73 @@ const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
 let score = 0;
+let userName= null
 /*this function load the quize i have used the 
 youtube tutorial https://www.youtube.com/watch?v=CqddbIrEM5I&t=73s to get the concept */
 
-quizLoad()
+function getUserName() {
+    let userNameInput = document.getElementById("user-name");
+    userName = userNameInput.value.trim();
+    let rulesText = document.getElementById("rules-text");
+    let errorMessage = document.getElementById("error-message");
+
+    // Code to enable/disable buttons, credit:
+  // https://stackdiary.com/enable-disable-button-javascript/
+  if (userName === "" || userName.length < 3 || userName.length > 10 || /\d/.test(userName)) {
+    userNameInput.value = "";
+    submitButton.disabled = true;
+    errorMessage.textContent = "Please enter a valid name (3-10 letters and no numbers)";
+    errorMessage.style.display = "block";
+    return;
+  } else {
+    errorMessage.style.display = "none";
+  };
+
+  rulesText.innerText = `So, ${userName}, the rules are pretty simple. 
+  This test contains 10 questions and you'll have to select one option among the four options provided.
+  In the end you will see how many you got it right.`;
+
+  submitButton.disabled = false;
+  errorMessage.style.display = "none";
+};
+
+
+// Submit button to load rules interface
+submitButton.addEventListener("click", () => {
+    startArea.classList.add("hide");
+    rulesArea.classList.remove("hide");
+  });
+  
+  submitButton.disabled = true; 
+  formInput.addEventListener("keyup", setButtonState);
+
+/**
+ * This function prevents the user to continue the game without filling in the input field.
+ * When the user clicks without filling in the input field, an alert message shows up.
+ */
+function setButtonState() {
+    if (document.querySelector(".form-input").value === "") {
+        submitButton.disabled = true;
+    } else {
+        submitButton.disabled = false;
+    };
+};
+
+/**
+ * This function resets the user name input to default state.
+ */
+
+// Rules Interface
+
+// Start quiz button to load quiz interface
+startQuiz.addEventListener("click", () => {
+  rulesArea.classList.add("hide");
+  questionsArea.classList.remove("hide");
+});  
+
+
+// Questions Interface
+quizLoad();
 function quizLoad() {
     deselectAnswers()
     const currentQuestionData = questionData[currentQuiz]
