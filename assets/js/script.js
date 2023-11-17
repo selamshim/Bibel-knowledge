5a9547ffd6b8991b360f5320476e3370dfd87cc9
selamshim <shimeselam@gmail.com>
2023-11-17 10:56:30 +0100

    commenting question area

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const startQuiz = document.getElementById("start-quiz-btn");
 const formInput = document.querySelector(".form-input");
 const questionsArea = document.getElementById("questions-area");
 
-//quuz part
+//quiz part
 const quiz= document.getElementById('quiz');
 const answerEls = document.querySelectorAll('.quizAnswer');
 const questionEl = document.getElementById('question');
@@ -27,7 +27,7 @@ let userName= null
 function UserNameValue() {
     let nameValue = document.getElementById("name");
     userName = nameValue.value.trim();
-    let rulesText = document.getElementById("rules-text");
+    let instructionText = document.getElementById("instruction-text");
     let errorMsg = document.getElementById("error-msg");
 
     // enable/disable buttons, tutorial : https://stackdiary.com/enable-disable-button-javascript/
@@ -42,7 +42,7 @@ function UserNameValue() {
     errorMsg.style.display = "none";
   };
 
-  rulesText.innerText = `The quiz contains 10 multiple choice questions and you need to select one option among the four options provided.
+  instructionText.innerText = `The quiz contains 10 multiple choice questions and you need to select one option among the four options provided.
    when you are done, you will see your score. good luck ${userName}`;
 
   submitButton.disabled = false;
@@ -71,12 +71,6 @@ function setButtonState() {
     };
 };
 
-/**
- * This function resets the user name input to default state.
- */
-
-// Rules Interface
-
 // Start quiz button to load quiz interface
 startQuiz.addEventListener("click", () => {
   instruction.classList.add("visibel");
@@ -89,47 +83,56 @@ startQuiz.addEventListener("click", () => {
 /*this function load the quize i have used the 
 youtube tutorial https://www.youtube.com/watch?v=CqddbIrEM5I&t=73s to get the concept */
 
-quizLoad();
+
+// Function to load the quiz
 function quizLoad() {
-    deselectAnswers()
-    const currentQuestionData = questionData[currentQuiz]
-    questionEl.innerText = currentQuestionData.question
+    deselectAnswers(); // Deselect any selected answers
+    const currentQuestionData = questionData[currentQuiz]; // Get data for the current question
+    // Display question and answer choices on the UI
+    questionEl.innerText = currentQuestionData.question;
     choice_a_text.innerText = currentQuestionData.a;
     choice_b_text.innerText = currentQuestionData.b;
     choice_c_text.innerText = currentQuestionData.c;
     choice_d_text.innerText = currentQuestionData.d;
-}
-function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-}
-
-function getSelected() {
-    let quizAnswer
+  }
+  
+  // Function to deselect all answer choices
+  function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+  }
+  
+  // Function to get the selected answer
+  function getSelected() {
+    let quizAnswer;
+    // Check which answer is selected by the user
     answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            quizAnswer = answerEl.id
-        }
-    })
-    return quizAnswer
-}
-//this will count the right answers
-submitBtn.addEventListener('click', () => {
-    const quizAnswer = getSelected()
-    if(quizAnswer) {
-       if(quizAnswer === questionData[currentQuiz].correct) {
-           score++
-       }
-       currentQuiz++
-       //this statement check if the question is finished
-       if(currentQuiz < questionData.length) {
-        quizLoad()
-       } 
-       //if the question is finished the score is displayed
-       else {
-           quiz.innerHTML = `
-           <h2>You scored ${score}/${questionData.length} . Enjoy Reading Bibel.</h2>
-           <button onclick="location.reload()">Reload</button>
-           `
-       }
+      if(answerEl.checked) {
+        quizAnswer = answerEl.id; // Record the ID of the selected answer
+      }
+    });
+    return quizAnswer; // Return the ID of the selected answer
+  }
+  
+  // Event listener for handling quiz submission
+  submitBtn.addEventListener('click', () => {
+    const quizAnswer = getSelected(); // Get the selected answer
+    if(quizAnswer) { // If an answer is selected
+      if(quizAnswer === questionData[currentQuiz].correct) { // If the selected answer is correct
+        score++; // Increment the score
+      }
+      currentQuiz++; // Move to the next question
+      if(currentQuiz < questionData.length) { // If there are more questions remaining
+        quizLoad(); // Load the next question
+      } else { // If all questions are answered
+        // Display the final score and a reload button
+        quiz.innerHTML = `
+          <h2>You scored ${score}/${questionData.length}. Enjoy Reading Bibel.</h2>
+          <button onclick="location.reload()">Reload</button>
+        `;
+      }
     }
-})
+  });
+  
+  // Initialize the quiz by loading the first question
+  quizLoad();
+  
